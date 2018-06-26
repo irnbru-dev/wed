d21772ed8a4a1eb1c84808147577a0d42023d357
Konovalova <darina.konovalova@it-grad.ru>
2018-06-26 18:15:39 +0300

    style

--- gulpfile.js
+++ gulpfile.js
@@ -5,21 +5,25 @@ var gulp = require('gulp'),
     concat = require('gulp-concat'),
     uglify = require('gulp-uglify'),
     uglifycss = require('gulp-uglifycss'),
+    svgSprite = require('gulp-svg-sprite'),
     LessPluginAutoPrefix = require('less-plugin-autoprefix'),
     autoprefix= new LessPluginAutoPrefix({browsers: ["last 20 versions"]});
 
 var path = {
     dest: {
         css:        'dist/css/',
-        js:         'dist/js/'
+        js:         'dist/js/',
+        svg:        'dist/svg'
     },
     src: {
         less:       'src/less/style.less',
-        js:         'src/js/*.js'
+        js:         'src/js/*.js',
+        svg:        'src/svg/*.svg'
     },
     watch: {
         less:       'src/less/style.less',
-        js:         'src/js/*.js'
+        js:         'src/js/*.js',
+        svg:        'src/svg/*.svg'
     }
 };
 
@@ -43,9 +47,25 @@ gulp.task('less', function () {
         .pipe(gulp.dest(path.dest.css))
 });
 
+gulp.task('svg', function () {
+    return gulp.src(path.src.svg)
+        .pipe(svgSprite({
+            selector: "icon-%f",
+            mode: {
+                css: { // Activate the «css» mode
+                    render: {
+                        css: true // Activate CSS output (with default options)
+                    }
+                }
+            }
+        }))
+        .pipe(gulp.dest(path.dest.svg));
+});
+
 gulp.task('watch', function() {
     gulp.watch(path.watch.less);
     gulp.watch(path.watch.js);
+    gulp.watch(path.watch.svg);
 });
 
 gulp.task('default', ['watch']);
